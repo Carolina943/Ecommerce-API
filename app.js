cf7d7b0cad0f9c66a7223d6474a0b389feb6b6b6
Carolina943 <akamifox943@gmail.com>
2022-10-14 12:38:15 -0400

    neW file

---
+++ app.js
@@ -0,0 +1,41 @@
+require('express-async-errors')
+//Express
+const express = require('express')
+const app = express()
+//dotenv
+const dotenv = require('dotenv')
+dotenv.config()
+
+//database
+const connectDB = require('./db/connect')
+
+//route
+
+app.get('/', (req, res)=>{
+  res.send('e-commerce API')
+});
+
+//middleware
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//app.use
+app.use(express.json())
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+//PORT
+const port = process.env.PORT
+
+const start = async()=>{
+  try{
+    await connectDB()
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  }catch(error){
+    console.log(error)
+  }
+}
+
+start()
