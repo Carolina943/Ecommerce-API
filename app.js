4bb2c1f70d75f32086e68ec49548582b8230330d
Carolina943 <akamifox943@gmail.com>
2022-10-21 13:04:26 -0400

    modified files

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
+const dotenv = require('dotenv')
+dotenv.config()
 require('express-async-errors')
+
 //Express
 const express = require('express')
 const app = express()
 //database
 const connectDB = require('./db/connect')
-//dotenv
-const dotenv = require('dotenv')
-dotenv.config()
+//routers
+const authRouter = require('./routes/authRoutes')
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //rest of the package
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser');
 
 //routers
-const authRouter = require('./routes/authRoutes')
-
-//route
 
 app.get('/', (req, res)=>{
   res.send('e-commerce API')
 });
 
-//app.use
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
 app.use('/api/v1/auth', authRouter)
 app.use(morgan('tiny'))
 
-//middleware
-
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,37 +1,55 @@
 const User = require('../models/User');
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const {createJWT} = require('../utils')
+const { attachCookiesToResponse} = require('../utils');
 
-const register = async(req,res)=>{
-  const {email, name, password} = req.body
-  const emailAlreadyExists =  await User.findOne({email})
-  if(emailAlreadyExists){
-    throw new CustomError.BadRequestError('Email already Exists')
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
   }
 
-  //first registered user is an again
-  const isFirstAccount = await User.countDocuments({}) === 0;
-  const role = isFirstAccount? 'admin': 'user';
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({email, name, password, role})
-  
-  const tokenUser = {name: user.name, userId: user._id, role:user.role}
-  const token = createJWT({ payload: tokenUser });
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = {name: user, userId: user._id, role: user.role}
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-  res.status(StatusCodes.CREATED).json({user: tokenUser, token});
-}
-
-const login = async(req,res)=>{
-    res.send('login user')
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
   }
+  const user = await User.findOne({ email });
 
-  const logout = async(req,res)=>{
-    res.send('logout user')
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  const tokenUser = {name: user.name, userId: user._id, role: user.role};
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+const logout = async (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+};
 
 module.exports = {
-  register, 
-  login, 
-  logout
-}
+  register,
+  login,
+  logout,
+};
