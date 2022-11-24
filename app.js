4901e5b5369f9e082093257c328eac89d12b3f68
Carolina943 <akamifox943@gmail.com>
2022-11-24 19:23:07 -0400

    modified and new files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config()
-require('express-async-errors')
+require('express-async-errors');
 
 //Express
 const express = require('express')
@@ -8,9 +8,9 @@ const app = express()
 //database
 const connectDB = require('./db/connect')
 //routers
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
-const productRouter =  require('./routes/productRoutes')
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter =  require('./routes/productRoutes');
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -18,7 +18,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 //rest of the package
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-
+const fileUpload = require('express-fileupload');
 //routers
 
 app.get('/', (req, res)=>{
@@ -27,6 +27,9 @@ app.get('/', (req, res)=>{
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static('./public'));
+app.use(fileUpload());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const path = require('path');
 
 const createProduct = async (req,res) =>{
   req.body.user = req.user.userId;
@@ -35,11 +36,39 @@ const updateProduct = async (req, res)=>{
 };
 
 const deleteProduct = async (req, res) =>{
-  res.send('delete product');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if(!product){
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  };
+  
+  await product.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Product removed. '});
 };
 
 const uploadImage = async (req,res) => {
-  res.send('upload image');
+  if(!req.files){
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+  const productImage = req.files.image;
+  if(!productImage.mimetype.startsWith('image')){
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  const maxSize = 1024*1024;
+
+  if(productImage.size > maxSize){
+    throw new CustomError.BadRequestError(
+    'Please upload image smaller than 1MB'
+    );
+  }
+  const imagePath = path.join(
+   __dirname, 
+   '../public/uploads/' + `${productImage.name}`
+   );
+   await productImage.mv(imagePath);
+   res.status(StatusCodes.OK).json({ image: `/public/uploads/${productImage.name}` });
 };
 
 
