ad96576ef6b96c03585f138f0e5b5a52b8ab9782
Carolina943 <akamifox943@gmail.com>
2022-11-24 14:02:14 -0400

    modified files

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const connectDB = require('./db/connect')
 //routers
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
+const productRouter =  require('./routes/productRoutes')
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //rest of the package
-const morgan = require('morgan')
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 //routers
@@ -27,13 +28,14 @@ app.get('/', (req, res)=>{
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 
 app.use(morgan('tiny'))
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 //PORT
 const port = process.env.PORT
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,4 +12,21 @@ const {
   updateProduct,
   deleteProduct,
   uploadImage,
-} = require('../controllers/productController')
+} = require('../controllers/productController');
+
+router
+.route('/')
+.post([authenticateUser,authorizePermissions('admin')], createProduct)
+.get(getAllProducts);
+
+router
+.route('/uploadImage')
+.post([authenticateUser, authorizePermissions('admin')], uploadImage);
+
+router
+.route('/:id')
+.get(getSingleProduct)
+.patch([authenticateUser,authorizePermissions('admin')], updateProduct)
+.delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+
+module.exports = router;
