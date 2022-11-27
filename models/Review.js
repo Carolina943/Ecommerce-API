39ebec2878cde595dfb071ff5f07f706b0825174
Carolina943 <akamifox943@gmail.com>
2022-11-27 12:49:58 -0400

    modified file

--- models/Review.js
+++ models/Review.js
@@ -33,12 +33,16 @@ const ReviewSchema = mongoose.Schema({
 // only one review per product per user
 ReviewSchema.index({ product: 1, user:1 }, {unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function(productId){
+  console.log(productId);
+};
+
 ReviewSchema.post('save', async function(){
-  console.log('post save hook called');
+  await this.constructor.calculateAverageRating(this.product);
 });
 
 ReviewSchema.post('remove', async function(){
-  console.log('post remove hool called');
+  await this.constructor.calculateAverageRating(this.product);
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
