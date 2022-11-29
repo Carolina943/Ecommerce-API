b0dd4709e12659601f49e8b8ed3991b7d1f845bc
Carolina943 <akamifox943@gmail.com>
2022-11-29 10:40:43 -0400

    modified and new files

--- app.js
+++ app.js
@@ -21,11 +21,24 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 //routers
 
-app.get('/', (req, res)=>{
-  res.send('e-commerce API')
-});
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
@@ -38,8 +51,6 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/review', reviewRouter);
 app.use('/api/v1/orders', orderRouter);
-
-app.use(morgan('tiny'))
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
